305820b7905a9c86dac287ed385801a4400c12da
Harsh Tiwari <akavaibhav231@gmail.com>
2025-07-25 13:15:42 +0530

    Add files via upload

---
+++ 1.code indoor.py
@@ -0,0 +1,9 @@
+#indoor.py
+
+#for user input 
+user_input = input("Please enter your message : ")
+
+#for lowercase all the letter user input 
+lowercase_input = user_input.lower()
+
+print(lowercase_input)
