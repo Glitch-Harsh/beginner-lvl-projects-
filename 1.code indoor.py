f71492dda71947ac0f7602a7aaa53f6d458619a6
Harsh Tiwari <akavaibhav231@gmail.com>
2025-07-25 13:18:01 +0530

    Delete 1.code indoor.py

--- 1.code indoor.py
+++
@@ -1,9 +0,0 @@
-#indoor.py
-
-#for user input 
-user_input = input("Please enter your message : ")
-
-#for lowercase all the letter user input 
-lowercase_input = user_input.lower()
-
-print(lowercase_input)
